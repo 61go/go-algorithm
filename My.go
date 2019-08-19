e37bdfaf1d0b9bd3b859f2c05b5ca70cd1ab6c7c
huruipeng <huruipeng@jd.com>
2019-08-19 09:06:20 +0800

    insertion sort

--- My.go
+++ My.go
@@ -1,11 +1,11 @@
 package main
 
 func main() {
-	
-	var x =[]int {3,4,1,8,7,2323,21,2,9}
+
+	var x = []int{3, 4, 1, 8, 7, 2323, 21, 2, 9}
 	sort(x)
-	for e,j := range x {
-		println(e,j)
+	for e, j := range x {
+		println(e, j)
 	}
 	//t1 := time.Now().Unix()
 	//var ii = factor(2, 12)
@@ -14,23 +14,21 @@ func main() {
 	//println(t2 - t1)
 }
 
-
 /**
-插入排序精要
+插入排序精要 插入排序是最简单的排序
  */
-func sort(x []int){
-	for i:= 1; i< len(x);i++  {
+func sort(x []int) {
+	for i := 1; i < len(x); i++ {
 		for j := i; j > 0; j-- {
 			if x[j-1] > x[j] {
-				x[j-1]^=x[j]
-				x[j]^=x[j-1]
-				x[j-1]^=x[j]
+				swap(x, j-1, j)
 			}
 		}
 	}
 }
+
 /**
-怎么算乘方
+怎么算乘方-0-0 乘方有效率的算法
  */
 func factor(x int, n int) int {
 	if n == 0 {
@@ -42,5 +40,13 @@ func factor(x int, n int) int {
 		tt := factor(x, n/2)
 		return tt * tt
 	}
+}
 
+/**
+算法:交互数组两个位置上的数值 只限于整数
+ */
+func swap(arr []int, left int, right int) {
+	arr[left] ^= arr[right]
+	arr[right] ^= arr[left]
+	arr[left] ^= arr[right]
 }
